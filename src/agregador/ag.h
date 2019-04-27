a40ce3bde7b99b4c0c1801ddd53ca5c11e7eb148
AsuosOnurb <brunoadns@gmail.com>
2019-04-27 19:36:07 +0100

    Reestruturação do projecto.

---
+++ src/agregador/ag.h
@@ -0,0 +1,8 @@
+//
+// Created by badns on 27-04-2019.
+//
+
+#ifndef SO1819_AG_H
+#define SO1819_AG_H
+
+#endif //SO1819_AG_H
---
+++ src/manutencao_artigos/ma.c
@@ -0,0 +1,5 @@
+//
+// Created by badns on 27-04-2019.
+//
+
+#include "ma.h"
