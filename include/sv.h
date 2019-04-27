abbca5e7edb0718cc27d79935a7ad4afd935bd14
AsuosOnurb <brunoadns@gmail.com>
2019-04-27 19:18:36 +0100

    Servidor de Vendas: Adicionada função que insere a entrada de uma venda no ficheiro VENDAS.txt .

---
+++ include/sv.h
@@ -0,0 +1,16 @@
+//
+// Created by badns on 27-04-2019.
+//
+
+#ifndef SO1819_SV_H
+#define SO1819_SV_H
+
+#endif //SO1819_SV_H
+
+/**
+ * Regista
+ * @param quantidade
+ * @param codigoArtigo
+ * @param precoArtigo
+ */
+ssize_t sv_registaVenda(int quantidade, char *codigoArtigo, double precoArtigo);
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/sv.h"
+
+int main() {
+    printf("Hello, World!\n");
+    sv_registaVenda(3, "A00001", 45.3);
+    return 0;
+}
