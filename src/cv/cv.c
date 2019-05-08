80ac49292a762df011d338e72d2bded39a7c7d3d
AsuosOnurb <brunoadns@gmail.com>
2019-05-08 16:37:12 +0100

    Adicionada função mostra_info_artigo() (cv)

--- src/cv/cv.c
+++ src/cv/cv.c
@@ -1,3 +1,30 @@
-
-
+#include <stdio.h>
 #include "cv.h"
+#include "../common/artigo.h"
+#include "../common/stock.h"
+
+void mostra_info_artigo(long codigoArtigo) {
+
+
+    artigo_t artigo;
+
+    int errorVal = artigo_load(codigoArtigo, &artigo);
+    if (errorVal < 0) {
+        printf("Erro: mostra_info_artigo():10 = %d\n", errorVal);
+        return;
+    }
+
+    printf("Preço do artigo: %f.\n", artigo->preco);
+    artigo_free(artigo);
+
+    stock_t stock;
+    errorVal = stock_load(codigoArtigo, &stock);
+    if (errorVal < 0) {
+        printf("Erro: mostra_info_artigo():23 = %d\n", errorVal);
+        return;
+    }
+
+    printf("Stock disponível: %ld.\n", stock->quantidade);
+    stock_free(stock);
+
+}
--- src/cv/cv.h
+++ src/cv/cv.h
@@ -2,4 +2,12 @@
 #ifndef SO1819_CV_H
 #define SO1819_CV_H
 
+/**
+ * Mostra no stdout o preço e o stock disponível do artigo com o código especificado.
+ * @param codigoArtigo O código do artigo cuja informação pretendemos mostrar.
+ */
+void mostra_info_artigo(long codigoArtigo);
+
+
+
 #endif //SO1819_CV_H
